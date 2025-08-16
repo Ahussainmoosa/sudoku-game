c78db491743e11755f0df19a1b91a9edb45f937f
Ahussainmoosa <m.3li.moosa@gmail.com>
2025-08-16 14:53:06 +0300

    complet the shape and most of the functionalty, still working on timer and pararmeter

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-const number =0;
+const value1 = document.getElementById('val4');
+const colorChange = document.querySelector('.box');
+const finshed = document.getElementById('Finsh');
+const final = document.getElementById('gameFinsh');
+const start =document.getElementById('Start');
+const time = document.getElementById('timer');
 
-number.addEventListener("input",)
+let insertedValue='';
+let secouns='0'
+value1.addEventListener('input', 
+    function() {
+        insertedValue = value1.value.trim(); 
+        if (insertedValue === '4') {
+            value1.classList.add('wright');
+            return insertedValue;
+        }
+        else if(insertedValue === ''){
+            value1.classList.remove('wright');
+            value1.classList.remove('wrong');
+        }else{
+            value1.classList.add('wrong')
+
+        }
+    });
+finshed.addEventListener("click",function(){
+    if(insertedValue==='4'){
+        final.innerHTML = "The game is finshed!";
+    }else{
+        final.innerHTML="ther is still emty blanc??";
+    }
+    
+})
+
+start.addEventListener("click",
+    function(){
+        for(let seconds=0;seconds<1000;seconds++){
+            final.innerHTML= "The is "+seconds;
+    }
+            
+})
